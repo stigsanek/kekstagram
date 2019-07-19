14f2f5e50ef3f94dc24a353204a5cdb9119f23cb
stigsanek <stigsanek@yandex.ru>
2019-07-19 15:32:52 +0400

    Добавляет фильтрацию фотографий других пользователей согласно ТЗ

--- js/main.js
+++ js/main.js
@@ -6,10 +6,13 @@
   var enablePage = function (responce) {
     window.page.render(responce, window.photo.create);
     window.filter.enable();
+    window.filter.employ(responce, window.page.render, window.photo.create, window.util.makeDebounce);
   };
 
   // Передаем модулую фотографий метод отрисовки и метод закрытия по Esc
   window.photo.initiate(window.page.render, window.util.pressEsc);
+  // Передаем модулю фильтров метод удаления элементов со страницы
+  window.filter.initiate(window.page.clear);
 
   document.addEventListener('DOMContentLoaded', function () {
     window.backend.download(enablePage, window.data.set);
--- js/util.js
+++ js/util.js
@@ -2,7 +2,23 @@
 
 // Модуль утилит
 (function () {
+  var DEBOUNCE_INTERVAL = 500;
   var ESC_KEYCODE = 27;
+
+  // Метод устранения дребезга
+  var doDebounce = function (cb) {
+    var lastTimeout = null;
+
+    return function () {
+      var parameters = arguments;
+      if (lastTimeout) {
+        window.clearTimeout(lastTimeout);
+      }
+      lastTimeout = window.setTimeout(function () {
+        cb.apply(null, parameters);
+      }, DEBOUNCE_INTERVAL);
+    };
+  };
 
   // Метод выполнения функций по нажатию ESC
   var pressEscKey = function (evt, action) {
@@ -12,6 +28,7 @@
   };
 
   window.util = {
+    makeDebounce: doDebounce,
     pressEsc: pressEscKey
   };
 })();
