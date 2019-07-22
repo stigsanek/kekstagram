03b254c36e86609f2b2fb82b76f6430d16b62e86
stigsanek <stigsanek@yandex.ru>
2019-07-22 13:11:28 +0400

    Добавляет логику изменения масштаба загруженного изображения

--- js/form.js
+++ js/form.js
@@ -3,6 +3,11 @@
 // Модуль формы
 (function () {
   var FILE_TYPE = ['gif', 'jpg', 'jpeg', 'png'];
+  var Scale = {
+    STEP: 25,
+    MIN: 25,
+    MAX: 100
+  };
 
   var formElement = document.querySelector('#upload-select-image');
   var formContainerElement = formElement.querySelector('.img-upload__overlay');
@@ -50,6 +55,32 @@
   };
 
   fileChoserElement.addEventListener('change', onFileChoserChange);
+
+  // Обработчики изменения масштаба изображения
+  var inputScaleElement = formElement.querySelector('.scale__control--value');
+  var smallBtnElement = formElement.querySelector('.scale__control--smaller');
+  var bigBtnElement = formElement.querySelector('.scale__control--bigger');
+
+  var countScale = parseInt(inputScaleElement.value, 10);
+
+  var onSmallBtnElementClick = function () {
+    if (countScale > Scale.MIN) {
+      countScale -= Scale.STEP;
+      inputScaleElement.value = countScale + '%';
+      previewElement.style = 'transform: scale(' + countScale/Scale.MAX + ');';
+    }
+  };
+
+  var onBigBtnElementClick = function () {
+    if (countScale < Scale.MAX && countScale >= Scale.MIN) {
+      countScale += Scale.STEP;
+      inputScaleElement.value = countScale + '%';
+      previewElement.style = 'transform: scale(' + countScale/Scale.MAX + ');';
+    }
+  };
+
+  smallBtnElement.addEventListener('click', onSmallBtnElementClick);
+  bigBtnElement.addEventListener('click', onBigBtnElementClick);
 
   window.form = {
     initiate: setFormMethod
