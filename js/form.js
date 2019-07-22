58b09b8ff478dafa1949ff87b2eb2cf2e04e4bd5
stigsanek <stigsanek@yandex.ru>
2019-07-22 11:46:56 +0400

    Добавляет возможность загрузки фотографии в форму редактирования изображения

--- js/form.js
+++ js/form.js
@@ -2,14 +2,33 @@
 
 // Модуль формы
 (function () {
+  var FILE_TYPE = ['gif', 'jpg', 'jpeg', 'png'];
+
   var formElement = document.querySelector('#upload-select-image');
   var formContainerElement = formElement.querySelector('.img-upload__overlay');
-  var uploadFileElement = formElement.querySelector('#upload-file');
+  var fileChoserElement = formElement.querySelector('#upload-file');
+  var previewElement = formElement.querySelector('.img-upload__preview').querySelector('img');
 
-  // Обработчик отображения формы редактирования изображения
-  var onUploadFileChange = function () {
-    formContainerElement.classList.remove('hidden');
+  // Обработчик загрузки изображения
+  var onFileChoserChange = function () {
+    var file = fileChoserElement.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPE.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        previewElement.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+      formContainerElement.classList.remove('hidden');
+    }
   };
 
-  uploadFileElement.addEventListener('change', onUploadFileChange);
+  fileChoserElement.addEventListener('change', onFileChoserChange);
 })();
